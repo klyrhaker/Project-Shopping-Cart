91969348fc24543c8dec79bb54b1e1fd987dd672
frst-repository <nikitagadlia123@gmail.com>
2026-06-29 23:22:16 +0300

    feat(cart-page): add CartPage with product list, totals, clear cart, and tests

---
+++ src/components/CartPage/CartPage.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router";
+import useCart from "../../hooks/useCart";
+import { getCartSummary } from "../../utils/cartUtils";
+import ProductCard from "../ProductCard/ProductCard";
+import Button from "../Button/Button";
+
+function CartPage() {
+  const { cart, clearCart } = useCart();
+  if (cart.length === 0) return <Link to="/shop">Перейти к товарам</Link>;
+  const { totalQuantity, totalPrice } = getCartSummary(cart);
+
+  return (
+    <>
+      {cart.map((product) => (
+        <ProductCard key={product.id} {...product} />
+      ))}
+
+      <p data-testid="total-price" className="total-price">
+        ${totalPrice.toFixed(2)}
+      </p>
+      <p data-testid="total-quantity" className="total-quantity">
+        {totalQuantity}
+      </p>
+      <Button className="clear-cart" onClick={clearCart}>
+        очистить корзину
+      </Button>
+    </>
+  );
+}
+export default CartPage;
---
+++ src/components/CartPage/CartPage.test.jsx
@@ -0,0 +1,159 @@
+import { render, screen } from "@testing-library/react";
+import useCart from "../../hooks/useCart";
+import userEvent from "@testing-library/user-event";
+import CartPage from "./CartPage";
+import { MemoryRouter } from "react-router";
+
+vi.mock("../../hooks/useCart");
+describe("CartPage", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+  test("при пустой корзине рендерит сслыку на страницу товаров", () => {
+    useCart.mockReturnValue({
+      cart: [],
+      addToCart: () => {},
+      decreaseFromCart: () => {},
+      clearCart: () => {},
+    });
+    render(
+      <MemoryRouter>
+        <CartPage />
+      </MemoryRouter>,
+    );
+    const link = screen.getByRole("link", { name: /перейти к товарам/i });
+    expect(link).toBeInTheDocument();
+  });
+  test("рендерит карточку товара из cart", () => {
+    useCart.mockReturnValue({
+      cart: [
+        {
+          title: "product1",
+          description: "",
+          id: 3,
+          img: { src: "/png", alt: "" },
+          price: 3,
+          quantity: 1,
+        },
+      ],
+      addToCart: () => {},
+      decreaseFromCart: () => {},
+      clearCart: () => {},
+    });
+    render(
+      <MemoryRouter>
+        <CartPage />
+      </MemoryRouter>,
+    );
+    const product1 = screen.getByRole("heading", { name: "product1" });
+    expect(product1).toBeInTheDocument();
+  });
+  test("рендерит общую цену всех товаров в корзине", () => {
+    useCart.mockReturnValue({
+      cart: [
+        {
+          title: "product1",
+          description: "",
+          id: 3,
+          img: { src: "/png", alt: "" },
+          price: 1,
+          quantity: 1,
+        },
+        {
+          title: "product2",
+          description: "",
+          id: 13,
+          img: { src: "/png", alt: "" },
+          price: 2,
+          quantity: 1,
+        },
+        {
+          title: "product3",
+          description: "",
+          id: 32,
+          img: { src: "/png", alt: "" },
+          price: 4,
+          quantity: 1,
+        },
+      ],
+      addToCart: () => {},
+      decreaseFromCart: () => {},
+      clearCart: () => {},
+    });
+    render(
+      <MemoryRouter>
+        <CartPage />
+      </MemoryRouter>,
+    );
+    const totalPrice = screen.getByTestId("total-price");
+    expect(totalPrice).toHaveTextContent("$7.00");
+  });
+  test("рендерит общее количество всех товаров в корзине", () => {
+    useCart.mockReturnValue({
+      cart: [
+        {
+          title: "product1",
+          description: "",
+          id: 3,
+          img: { src: "/png", alt: "" },
+          price: 1,
+          quantity: 1,
+        },
+        {
+          title: "product2",
+          description: "",
+          id: 13,
+          img: { src: "/png", alt: "" },
+          price: 2,
+          quantity: 5,
+        },
+        {
+          title: "product3",
+          description: "",
+          id: 32,
+          img: { src: "/png", alt: "" },
+          price: 4,
+          quantity: 2,
+        },
+      ],
+      addToCart: () => {},
+      decreaseFromCart: () => {},
+      clearCart: () => {},
+    });
+    render(
+      <MemoryRouter>
+        <CartPage />
+      </MemoryRouter>,
+    );
+    const totalQuantity = screen.getByTestId("total-quantity");
+    expect(totalQuantity).toHaveTextContent("8");
+  });
+  test("при нажатии на кнопку очистки корзины вызывается clearCart", async () => {
+    const clearCart = vi.fn();
+    useCart.mockReturnValue({
+      cart: [
+        {
+          title: "product1",
+          description: "",
+          id: 3,
+          img: { src: "/png", alt: "" },
+          price: 1,
+          quantity: 1,
+        },
+      ],
+      addToCart: () => {},
+      decreaseFromCart: () => {},
+      clearCart,
+    });
+
+    render(
+      <MemoryRouter>
+        <CartPage />
+      </MemoryRouter>,
+    );
+    const button = screen.getByRole("button", { name: /очистить корзину/i });
+    const user = userEvent.setup();
+    await user.click(button);
+    expect(clearCart).toHaveBeenCalled();
+  });
+});
